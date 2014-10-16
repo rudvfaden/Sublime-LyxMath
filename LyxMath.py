48b6182e13c2935dde26c67a0f70edb56f31648e
Rud Faden <rudfaden@gmail.com>
2014-10-16 12:28:58 +0200

    Initial Commit

---
+++ LyxMath.py
@@ -0,0 +1,30 @@
+import sublime, sublime_plugin
+
+class DisplayMathCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+    	# Walk through each region in the selection
+        for region in self.view.sel():
+        	# Only interested in empty regions, otherwise they may span multiple  
+            # lines, which doesn't make sense for this command.
+            if region.empty():
+            	# Expand the region to the full line it resides on, excluding the newline
+            	line = self.view.line(region)
+            	# Extract the line content
+            	line_contents = self.view.substr(line)
+            	# Check if the line content is empty of just space and tabs. 
+            	# If empty/space/tabs insert only one new line. 
+            	# else inser two
+            	if (not line_contents or line_contents.isspace()):
+            		self.view.run_command("insert_snippet",{"contents": "\n\\[\n\t$1\n\\]"})
+            	else:
+            		self.view.run_command("insert_snippet",{"contents": "\n\n\\[\n\t$1\n\\]"})
+
+class InlineMathCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		# Walk through each region in the selection
+		for region in self.view.sel():
+			# Only interested in empty regions, otherwise they may span multiple  
+            # lines, which doesn't make sense for this command.
+			if region.empty():
+				self.view.run_command("insert_snippet",{"contents": "\\$$1\\$"})
+
